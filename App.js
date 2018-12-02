7ff18a6cdc61bd6727f497772764fd57d1b0b761
ansnoussi <anis0snoussi@gmail.com>
2018-12-02 17:20:10 +0000

    part1

--- App.js
+++ App.js
@@ -2,20 +2,46 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state={};
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operations}></View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1
+    },
+    result: {
+      flex: 2,
+      backgroundColor: 'red'
+    },
+    calculation: {
+      flex: 1,
+      backgroundColor: 'green'
+    },
+    buttons: {
+      flex: 7,
+      flexDirection: 'row'
+    },
+    numbers: {
+      flex: 3,
+      backgroundColor: 'yellow'
+    },
+    operations: {
+      flex: 1,
+      backgroundColor: 'black'
+    }
 });
